46905c8294e594e97132776f16eba45956a826d2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-04 15:20:43 +0530

    input text state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
+import React, { useState } from 'react';
 import Form from './components/Form';
 import Header from './components/Header';
 import TodoList from './components/TodoList';
+import './App.css';
 
 function App() {
+    const [inputText, setInputText] = useState('');
+
     return (
         <div className="App">
             <Header />
-            <Form />
-            <TodoList />
+            <Form setInputText={setInputText} />
+            <TodoList inputText={inputText} />
         </div>
     );
 }
