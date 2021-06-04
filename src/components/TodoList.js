9a056a1ba7ebb3a3eef65a1fb3dc26e2bdd35e5d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-04 15:42:14 +0530

    mapping throgh todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Todo from './Todo';
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                {todos.map(todo => (
+                    <Todo key={todo.id} text={todo.text} />
+                ))}
+            </ul>
         </div>
     )
 }
