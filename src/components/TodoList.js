3d5627390227870fd418dc5f6fb0c44a2c2ebba1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-04 15:48:21 +0530

    passing setTodos to todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Todo from './Todo';
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
     return (
         <div className="todo-container">
             <ul className="todo-list">
                 {todos.map(todo => (
-                    <Todo key={todo.id} text={todo.text} />
+                    <Todo key={todo.id} text={todo.text} setTodos={setTodos} />
                 ))}
             </ul>
         </div>
