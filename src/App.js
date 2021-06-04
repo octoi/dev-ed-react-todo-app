60df10a8befa1ffced68de8488c02ebb38340f26
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-04 15:11:30 +0530

    used TodoList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Form from './components/Form';
 import Header from './components/Header';
+import TodoList from './components/TodoList';
 
 function App() {
     return (
         <div className="App">
             <Header />
             <Form />
+            <TodoList />
         </div>
     );
 }
