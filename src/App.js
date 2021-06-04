ec25f50336713d008f9e1aec69f3c0a0f59d86e1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-04 15:32:34 +0530

    todos states

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import './App.css';
 
 function App() {
     const [inputText, setInputText] = useState('');
+    const [todos, setTodos] = useState([]);
 
     return (
         <div className="App">
             <Header />
-            <Form setInputText={setInputText} />
+            <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
             <TodoList inputText={inputText} />
         </div>
     );
